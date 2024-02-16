8daf2033b62fe1be4297f0458befc67ec762be98
MinhDuc <frostgamer150@gmail.com>
2024-02-16 16:02:46 +0700

    first starter project

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import pygame
 import time
 import random
+pygame.font.init()
 
-WIDTH, HEIGHT = 1200, 800
+WIDTH, HEIGHT = 700, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space-man")
 
-# BG = pygame.image.load("yellow-diagonal-geometric-striped-background-with-halftone-detailed_1409-1451.jpeg")
+BG = pygame.image.load("Background-clouds.jpg")
 
 PLAYER_WIDTH = 40
 PLAYER_HEIGHT = 60
 PLAYER_VEL = 5
+STAR_WIDTH = 10
+STAR_HEIGHT = 10
+FONT = pygame.font.SysFont("comicsmas", 30)
+STAR_VEL = 3
 
-def draw(player):
-    # WIN.blit(BG, (0,0))
+def draw(player, elapsed_time, stars):
+    WIN.blit(BG, (0,0))
 
+    time_text = FONT.render(f"time: {round(elapsed_time)}s", 1, "white")
+    WIN.blit(time_text, (10, 10))
     pygame.draw.rect(WIN, (255, 0, 0), player)
 
+    for star in stars:
+        pygame.draw.rect(WIN, "white", star)
+        
     pygame.display.update()
 
 def main():
@@ -24,11 +34,31 @@ def main():
 
     player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
 
-    clock = pygame.time.Clock() # set the speed to be the same regardless of computer type,..
+    clock = pygame.time.Clock() # create a clock object
 
+    start_time = time.time()
+    
+    elapsed_time = 0
+    
+    star_add_increment = 2000
+    star_count = 0
+    
+    stars = []
+    hit = False
+    
     while run:
-        clock.tick(60) # only run 60 times per sec
+        star_count += clock.tick(60)
+        elapsed_time = time.time() - start_time
 
+        if star_count > star_add_increment:
+            for _ in range(3):
+                star_x = random.randint(0, WIDTH - STAR_WIDTH)
+                star = pygame.Rect(star_x, -STAR_HEIGHT, STAR_WIDTH, STAR_HEIGHT)
+                stars.append(star)
+                
+            star_add_increment = max(200, star_add_increment - 50)
+            star_count = 0
+            
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -40,7 +70,16 @@ def main():
         if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + player.width <= WIDTH:
             player.x += PLAYER_VEL
 
-        draw(player)
+        for star in stars[:]:
+            star.y += STAR_VEL
+            if star.y > HEIGHT:
+                stars.remove(star)
+                
+            elif star.y >= player.y and star.colliderect(player):
+                stars.remove(star)
+                hit = True
+                break
+        draw(player, elapsed_time, stars)
 
     pygame.quit()
 
--- test.py
+++ test.py
@@ -1,26 +1,8 @@
-import sys, pygame
-from pygame.locals import *
-import os
-pygame.init()
+import pygame
 
-size = width, height = 320, 240
-speed = [2,2]
-black = 0, 0, 0
-screen = pygame.display.set_mode(size) # create a graphical window
+WIDTH = 800
+HEIGHT = 500
+game_sur = pygame.display.set_mode((WIDTH, HEIGHT))
+game_cap = pygame.display.set_caption("a game")
 
-ball = pygame.image.load(os.path.join('pygame', 'yellow-diagonal-geometric-striped-background-with-halftone-detailed_1409-1451.gif'))
-ballrect = ball.get_rect()
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
-
-    screen.fill(black)
-    screen.blit(ball, ballrect)
-    pygame.display.flip()
+sur_sample = pygame.display.get_surface()
